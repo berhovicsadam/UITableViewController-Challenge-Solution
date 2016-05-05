104958d2dd9d82821c62fd99ede338657c223c67
Berhovics Ádám <berhovicsadam123@icloud.com>
2016-05-05 14:05:18 +0200

    Initial Commit

---
+++ UITabelViewController Challange Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITabelViewController Challange Solution
+//
+//  Created by Berhovics Ádám on 05/05/16.
+//  Copyright © 2016 PGcomp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITabelViewController Challange Solution/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITabelViewController Challange Solution
+//
+//  Created by Berhovics Ádám on 05/05/16.
+//  Copyright © 2016 PGcomp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
